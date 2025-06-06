a8433cbb108afcace6a1259aa18007c76e1a305c
vishal kushwaha <vishal.kushwaha@infra.market>
2025-06-06 09:17:39 +0530

    optimization

--- src/features/cart/CartItem/index.tsx
+++ src/features/cart/CartItem/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, memo } from "react";
+import React, { useCallback } from "react";
 import { useTranslation } from "react-i18next";
 import { useDispatch } from "react-redux";
 
@@ -60,4 +60,4 @@ const CartItem: React.FC<IProduct> = (props) => {
   );
 };
 
-export default CartItem;
+export default React.memo(CartItem);
--- src/features/shop/Product/index.tsx
+++ src/features/shop/Product/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, memo } from "react";
+import React, { useCallback } from "react";
 import { useTranslation } from "react-i18next";
 import { useDispatch } from "react-redux";
 
